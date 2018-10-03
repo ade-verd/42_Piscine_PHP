15bb3bc6a8d97d25d9764b438e63b2a55720749b
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-03 20:45:04 +0200

    d01 ex13

---
+++ d01/ex13/sing_it!.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+	if ($argc == 2)
+	{
+		$rep = "Tout simplement pour qu'en feuilletant le sujet\non ne s'apercoive pas de la nature de l'exo..\n";
+		if ($argv[1] == "mais pourquoi cette demo ?")
+			echo $rep;
+	}
+?>
