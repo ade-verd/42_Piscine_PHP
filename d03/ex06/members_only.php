6dd4fcef3c93d6b3cb9c0bb21ec6a03e009bc5e9
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-04 21:17:48 +0200

    d03 ex06

---
+++ d03/ex06/members_only.php
@@ -0,0 +1,28 @@
+<?php
+	function get_data_img($path)
+	{
+		return base64_encode(file_get_contents($path));
+	}
+
+	$img = "../img/42.png";
+	$valid_user = "zaz";
+	$valid_password = "jaimelespetitsponeys";
+
+	$user = $_SERVER['PHP_AUTH_USER'];
+	$pass = $_SERVER['PHP_AUTH_PW'];
+	$valid = ($user == $valid_user && $pass == $valid_password);
+
+	if (!$valid)
+	{
+		header('WWW-Authenticate: Basic realm="My Realm"');
+		header('HTTP/1.0 401 Unauthorized');
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+	}
+	else
+	{
+		echo "<html><body>\n";
+		echo "Bonjour Zaz<br />\n";
+		echo "<img src='data:image/png;base64,".get_data_img($img)."'>\n";
+		echo "</body></html>\n";
+	}
+?>
