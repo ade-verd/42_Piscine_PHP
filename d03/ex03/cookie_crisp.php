3c4fa071a4fff9e3ca29626bd401270a5189ebd4
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-04 19:42:49 +0200

    d03 ex03

---
+++ d03/ex03/cookie_crisp.php
@@ -0,0 +1,29 @@
+<?php
+	function set_cookie()
+	{
+		if ($_GET['name'] && $_GET['value'])
+			setcookie($_GET['name'], $_GET['value'], time() + 3600, "/", "localhost");
+	}
+
+	function get_cookie()
+	{
+		if ($_GET['name'] && $_COOKIE[$_GET['name']])
+			echo $_COOKIE[$_GET['name']]."\n";
+	}
+
+	function del_cookie()
+	{
+		if ($_GET['name'])
+			setcookie($_GET['name'], '', time() - 3600, "/", "localhost");
+	}
+
+	if ($_GET['action'])
+	{
+		if ($_GET['action'] == "set")
+			set_cookie();
+		elseif ($_GET['action'] == "get")
+			get_cookie();
+		elseif ($_GET['action'] == "del")
+			del_cookie();
+	}
+?>
