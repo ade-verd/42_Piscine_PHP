6d34d7ec127c79853ccc5a28967b2bd9eddf3a28
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 12:52:45 +0200

    d05 ex02

--- d05/ex02/ex02.sql
+++ d05/ex02/ex02.sql
@@ -3,14 +3,15 @@
 ** mysql -u root -p db_ade-verd < resources/base-student.sql
 */
 
-CREATE TABLE `ft_tables` (
-				`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-				`login` VARCHAR(8) DEFAULT 'toto' NOT NULL,
-				`group` ENUM('staff', 'student', 'other') NOT NULL,
-				`creation_date` DATE NOT NULL); 
+INSERT INTO `ft_table` (`login`, `group`, `creation_date`)
+VALUES	('loki', 'staff', '2013-05-01'),
+		('scadoux', 'student', '2014-01-01'),
+		('chap', 'staff', '2011-04-27'),
+		('bamboo', 'staff', '2014-03-01'),
+		('fantomet', 'staff', '2010-04-03');
 
 /*
 ** How to check:
-** mysql -u root -p db_ade-verd < ex01.sql
-** mysql -u root -p db_ade-verd -e "DESCRIBE ft_tables;"
+** mysql -u root -p db_ade-verd < ex02.sql
+** mysql -u root -p db_ade-verd -e "SELECT * FROM ft_table;"
 */
