e3495c33330a11c1fa476781b7cea52352f2c1d1
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 12:39:52 +0200

    d05 ex01

--- d05/ex00/ex00.sql
+++ d05/ex00/ex00.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE `db_ade-verd`;
+
+/*
+** How to check:
+** mysql -u root -p < ex00.sql
+** mysql -u root -p -e "SHOW DATABASES;"
+*/
---
+++ d05/ex02/ex02.sql
@@ -0,0 +1,16 @@
+/*
+** Run before:
+** mysql -u root -p db_ade-verd < resources/base-student.sql
+*/
+
+CREATE TABLE `ft_tables` (
+				`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+				`login` VARCHAR(8) DEFAULT 'toto' NOT NULL,
+				`group` ENUM('staff', 'student', 'other') NOT NULL,
+				`creation_date` DATE NOT NULL); 
+
+/*
+** How to check:
+** mysql -u root -p db_ade-verd < ex01.sql
+** mysql -u root -p db_ade-verd -e "DESCRIBE ft_tables;"
+*/
