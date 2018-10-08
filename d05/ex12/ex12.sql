8796707b2842153166c2a244af8afe6eeb13fe10
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 16:31:41 +0200

    day 05 ex12

--- d05/ex12/ex12.sql
+++ d05/ex12/ex12.sql
@@ -1,4 +1,9 @@
-
+SELECT last_name, first_name FROM user_card
+WHERE
+	lower(last_name) REGEXP '[^a-z]+'
+	OR
+	lower(first_name) REGEXP '[^a-z]+'
+ORDER BY last_name ASC, first_name ASC;
 /*
 ** How to check:
 ** mysql -u root -p db_ade-verd < ex##.sql
