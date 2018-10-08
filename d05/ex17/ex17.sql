30a9f2daae69d58a17ef8b256846f0cc1873dfeb
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 18:58:50 +0200

    d05 ex18

--- d05/ex17/ex17.sql
+++ d05/ex17/ex17.sql
@@ -3,7 +3,6 @@ SELECT 	COUNT(id_member) AS 'nb_susc',
 		MOD(SUM(duration_sub), 42) AS 'ft' FROM member
 INNER JOIN subscription ON member.id_sub = subscription.id_sub;
 
-
 /*
 ** How to check:
 ** mysql -u root -p db_ade-verd < ex##.sql
--- d05/ex18/ex18.sql
+++ d05/ex18/ex18.sql
@@ -1,3 +1,9 @@
+SELECT `name` FROM distrib
+WHERE
+	id_distrib REGEXP '^(42|6[2-9]|71|88|89|90)$'
+	OR
+	`name` REGEXP '(.*[yY].*){2}'
+LIMIT 2, 5;
 
 /*
 ** How to check:
