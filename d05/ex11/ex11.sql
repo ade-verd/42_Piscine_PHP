488bc335a6b9b09ef34b516576b1058474587476
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 16:07:38 +0200

    day 05 ex11

--- d05/ex11/ex11.sql
+++ d05/ex11/ex11.sql
@@ -1,3 +1,9 @@
+SELECT UPPER(last_name) AS "NAME", first_name, price FROM user_card
+INNER JOIN member ON user_card.id_user = member.id_user_card
+INNER JOIN subscription ON subscription.id_sub = member.id_sub
+WHERE price > 42
+ORDER BY last_name ASC, first_name ASC;
+
 
 /*
 ** How to check:
