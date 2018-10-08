a0847ca4aa4e17e6a55a8d305392b3c086998d6d
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 15:49:20 +0200

    day 05 ex10

--- d05/ex10/ex10.sql
+++ d05/ex10/ex10.sql
@@ -1,3 +1,7 @@
+SELECT title AS 'Title', summary AS 'Summary', prod_year FROM film
+WHERE
+	id_genre = (SELECT id_genre FROM genre WHERE `name` = 'erotic')
+ORDER BY prod_year DESC;
 
 /*
 ** How to check:
