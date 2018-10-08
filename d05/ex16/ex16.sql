720c9a6400d9108a990a41bdd8ff6a68aefff051
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-08 17:54:12 +0200

    day 05 ex16

--- d05/ex16/ex16.sql
+++ d05/ex16/ex16.sql
@@ -1,3 +1,7 @@
+SELECT * FROM program_grid
+WHERE
+	(beginning_show >= "2006-10-30" AND beginning_show <= "2007-07-27")
+	OR (MONTH(beginning_show) = 12 AND DAY(beginning_show) = 24);
 
 /*
 ** How to check:
