355af363b9e720cf17f182229b72575904951295
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-09 12:34:14 +0200

    ex16 modify request distinct films

--- d05/ex16/ex16.sql
+++ d05/ex16/ex16.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(*) FROM member_history
+SELECT COUNT(DISTINCT CONCAT(id_film, `date`)) FROM member_history
 WHERE
 	(`date` >= "2006-10-30" AND `date` <= "2007-07-27")
 	OR (MONTH(`date`) = 12 AND DAY(`date`) = 24);
--- d05/ex21/ex21.sql
+++ d05/ex21/ex21.sql
@@ -1,7 +1,6 @@
 SELECT MD5(REPLACE(CONCAT(phone_number, '42'), '7', '9')) AS 'ft5' FROM distrib
 WHERE id_distrib = 84;
 
-
 /*
 ** How to check:
 ** mysql -u root -p db_ade-verd < ex##.sql
