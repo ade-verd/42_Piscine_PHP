af66cce3142cbcf92564b7a7d18c6dae323ebfe9
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-09 18:32:52 +0200

    d06 ex00 add

--- d06/ex00/Color.class.php
+++ d06/ex00/Color.class.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once 'Color.doc.php';
-
 class Color {
 
-	public static $verbose = FALSE;
+	public static $verbose = False;
 
 	public $red = 0;
 	public $green = 0;
@@ -28,32 +26,48 @@ class Color {
 			print(self::__toString() . " constructed." . PHP_EOL);
 	}
 
+	public function __destruct() {
+		if (self::$verbose == TRUE)
+			print(self::__toString() . " destructed." . PHP_EOL);
+	}
+
 	public function __toString() {
 		$s = sprintf ("Color( red: %3d, green: %3d, blue: %3d )", $this->getRed(), $this->getGreen(), $this->getBlue());
 		return $s;
+	}
+
+	public function add($class) {
+		$array = array();
+		$array['red'] = $this->getRed() + $class->getRed();
+		$array['green'] = $this->getGreen() + $class->getGreen();
+		$array['blue'] = $this->getBlue() + $class->getBlue();
+		return new Color($array);
 	}
 
 	public function getRed() { return $this->red; }
 	public function getGreen() { return $this->green; }
 	public function getBlue() { return $this->blue; }
 
-	private function _setRed($newval) { 
+	private function _setRed($newval) {
+		$newval = intval($newval); 
 		if ($newval >= 0 && $newval <= 255)
 			$this->red = $newval;
 		return;
 	}
 	private function _setGreen($newval) { 
+		$newval = intval($newval); 
 		if ($newval >= 0 && $newval <= 255)
 			$this->green = $newval;
 		return;
 	}
 	private function _setBlue($newval) { 
+		$newval = intval($newval); 
 		if ($newval >= 0 && $newval <= 255)
 			$this->blue = $newval;
 		return;
 	}
 	
-	public static function doc() { print_doc(); }
+	public static function doc() { return file_get_contents("Color.doc.txt"); }
 
 }
 
--- d06/ex00/test.php
+++ d06/ex00/test.php
@@ -4,17 +4,25 @@
 	Color::$verbose = True;
 
 #	print( Color::doc() );
+
 	$red     = new Color( array( 'red' => 0xff, 'green' => 0   , 'blue' => 0    ) );
 	$green   = new Color( array( 'rgb' => 255 << 8 ) );
 	$blue    = new Color( array( 'red' => 0   , 'green' => 0   , 'blue' => 0xff ) );
+	
+	$yellow  = $red->add( $green );
+	$cyan    = $green->add( $blue );
+	$magenta = $blue->add( $red );
+
+	$white   = $red->add( $green )->add( $blue );
 
 	print( $red     . PHP_EOL );
 	print( $green   . PHP_EOL );
 	print( $blue    . PHP_EOL );
-//	print( $yellow  . PHP_EOL );
-//	print( $cyan    . PHP_EOL );
-//	print( $magenta . PHP_EOL );
-//	print( $white   . PHP_EOL );
+
+	print( $yellow  . PHP_EOL );
+	print( $cyan    . PHP_EOL );
+	print( $magenta . PHP_EOL );
+	print( $white   . PHP_EOL );
 
 
 ?>
