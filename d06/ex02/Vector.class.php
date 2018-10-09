19eef624ec4b69e5051a3313b6d0d125ee04d24b
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-09 20:44:48 +0200

    d06 ex02 in progress

---
+++ d06/ex02/Vector.class.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once 'Color.class.php';
+
+class Vector {
+
+	public static $verbose = False;
+
+	private $_x = 0.0;
+	private $_y = 0.0;
+	private $_z = 0.0;
+	private $_w = 0.0;
+
+	public function __construct(array $kwargs) {
+		if (array_key_exists('x', $kwargs))
+			$this->setX($kwargs['x']);
+		if (array_key_exists('y', $kwargs))
+			$this->setY($kwargs['y']);
+		if (array_key_exists('z', $kwargs))
+			$this->setZ($kwargs['z']);
+		if (array_key_exists('w', $kwargs))
+			$this->setW($kwargs['w']);
+		if (array_key_exists('color', $kwargs))
+			$this->setColor($kwargs['color']);
+		else
+			$this->setColor(new Color(array('red' => 255, 'green' => 255, 'blue' => 255)));
+		if (self::$verbose == TRUE)
+			print(self::__toString() . " constructed" . PHP_EOL);
+	}
+
+	public function __destruct() {
+		if (self::$verbose == TRUE)
+			print(self::__toString() . " destructed" . PHP_EOL);
+	}
+
+	public function __toString() {
+		$color_verb = "";
+		if (self::$verbose == TRUE)
+			$color_verb = ", ".$this->getColor();
+		$s = sprintf ("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f%s )",
+			$this->getX(), $this->getY(), $this->getZ(), $this->getW(), $color_verb);
+		return $s;
+	}
+
+	public function getX() { return $this->_x; }
+	public function getY() { return $this->_y; }
+	public function getZ() { return $this->_z; }
+	public function getW() { return $this->_w; }
+
+	private function _setX($newval) {
+		$this->_x = $newval;
+		return;
+	}
+
+	private function _setY($newval) {
+		$this->_y = $newval;
+		return;
+	}
+	
+	private function _setZ($newval) {
+		$this->_z = $newval;
+		return;
+	}
+	
+	private function _setW($newval) {
+		$this->_w = $newval;
+		return;
+	}
+	
+	public static function doc() { return file_get_contents("Vector.doc.txt"); }
+
+}
+
+?>
