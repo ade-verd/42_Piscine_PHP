6f2314082e4dedd5973b7670ddb8d74437feb896
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-09 16:07:02 +0200

    construct rgb

--- d06/ex00/Color.class.php
+++ d06/ex00/Color.class.php
@@ -6,33 +6,49 @@ class Color {
 
 	public $verbose = FALSE;
 
-	private $_red = 0;
-	private $_green = 0;
-	private $_blue = 0;
+	public $red = 0;
+	public $green = 0;
+	public $blue = 0;
 
 	public function __construct(array $kwargs) {
 		if (array_key_exists('rgb', $kwargs)) {
-			if (array_key_exists('red', $kwargs['rgb']))
-				$this->_red = $kwargs['rgb']['red'];
-			if (array_key_exists('green', $kwargs['rgb']))
-				$this->_green = $kwargs['rgb']['green'];
-			if (array_key_exists('blue', $kwargs['rgb']))
-				$this->_blue = $kwargs['rgb']['blue'];
+			// C = 256^2 * R + 256 * G + B;
+			$this->_setRed($kwargs['rgb'] / (256 * 256));
+			$this->_setGreen(($kwargs['rgb'] / 256) % 256);
+			$this->_setBlue($kwargs['rgb'] % 256);
+			printf ("Color( red: %3d, green: %3d, blue: %3d ) constructed.\n", $this->getRed(), $this->getGreen(), $this->getBlue());
 		} else {
 			if (array_key_exists('red', $kwargs))
-				$this->_red = $kwargs['red'];
+				$this->_setRed($kwargs['red']);
 			if (array_key_exists('green', $kwargs))
-				$this->_green = $kwargs['green'];
+				$this->_setGreen($kwargs['green']);
 			if (array_key_exists('blue', $kwargs))
-				$this->_blue = $kwargs['blue'];
+				$this->_setBlue($kwargs['blue']);
+			printf ("Color( red: %3d, green: %3d, blue: %3d ) constructed.\n", $this->getRed(), $this->getGreen(), $this->getBlue());
 		}
 	}
 
-	public function getRed() { return $this->_red; }
-	public function getGreen() { return $this->_Green; }
-	public function getBlue() { return $this->_Blue; }
+	public function getRed() { return $this->red; }
+	public function getGreen() { return $this->green; }
+	public function getBlue() { return $this->blue; }
 
-	public function doc() { print_doc(); }
+	private function _setRed($newval) { 
+		if ($newval >= 0 && $newval <= 255)
+			$this->red = $newval;
+		return;
+	}
+	private function _setGreen($newval) { 
+		if ($newval >= 0 && $newval <= 255)
+			$this->green = $newval;
+		return;
+	}
+	private function _setBlue($newval) { 
+		if ($newval >= 0 && $newval <= 255)
+			$this->blue = $newval;
+		return;
+	}
+	
+	private function doc() { print_doc(); }
 
 }
 
--- d06/ex00/test.php
+++ d06/ex00/test.php
@@ -2,5 +2,8 @@
 
 	require_once 'Color.class.php';
 
-	print( Color::doc() );
+#	print( Color::doc() );
+	$red     = new Color( array( 'red' => 0xff, 'green' => 0   , 'blue' => 0    ) );
+	$green   = new Color( array( 'rgb' => 255 << 8 ) );
+	$blue    = new Color( array( 'red' => 0   , 'green' => 0   , 'blue' => 0xff ) );
 ?>
