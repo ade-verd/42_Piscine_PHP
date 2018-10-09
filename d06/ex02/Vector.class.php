bdcb1c78595f6768789195671ba04a4c3f693de3
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-09 22:02:25 +0200

    d06 ex02 magnitude

--- d06/ex02/Vector.class.php
+++ d06/ex02/Vector.class.php
@@ -10,20 +10,18 @@ class Vector {
 	private $_y = 0.0;
 	private $_z = 0.0;
 	private $_w = 0.0;
+	private $_dest;
+	private $_orig;
 
 	public function __construct(array $kwargs) {
-		if (array_key_exists('x', $kwargs))
-			$this->setX($kwargs['x']);
-		if (array_key_exists('y', $kwargs))
-			$this->setY($kwargs['y']);
-		if (array_key_exists('z', $kwargs))
-			$this->setZ($kwargs['z']);
-		if (array_key_exists('w', $kwargs))
-			$this->setW($kwargs['w']);
-		if (array_key_exists('color', $kwargs))
-			$this->setColor($kwargs['color']);
+		if (array_key_exists('orig', $kwargs))
+			$this->_orig = $kwargs['orig'];
 		else
-			$this->setColor(new Color(array('red' => 255, 'green' => 255, 'blue' => 255)));
+			$this->_orig = new Vertex(array('x' => 0.0, 'y' => 0.0, 'z' => 0.0, 'w' => 1.0));
+		if (array_key_exists('dest', $kwargs)) {
+			$this->_dest = $kwargs['dest'];
+			$this->_diff_vertex();
+		}
 		if (self::$verbose == TRUE)
 			print(self::__toString() . " constructed" . PHP_EOL);
 	}
@@ -34,11 +32,8 @@ class Vector {
 	}
 
 	public function __toString() {
-		$color_verb = "";
-		if (self::$verbose == TRUE)
-			$color_verb = ", ".$this->getColor();
-		$s = sprintf ("Vertex( x: %.2f, y: %.2f, z:%.2f, w:%.2f%s )",
-			$this->getX(), $this->getY(), $this->getZ(), $this->getW(), $color_verb);
+		$s = sprintf ("Vector( x:%.2f, y:%.2f, z:%.2f, w:%.2f )",
+			$this->getX(), $this->getY(), $this->getZ(), $this->getW());
 		return $s;
 	}
 
@@ -47,23 +42,16 @@ class Vector {
 	public function getZ() { return $this->_z; }
 	public function getW() { return $this->_w; }
 
-	private function _setX($newval) {
-		$this->_x = $newval;
-		return;
+	public function magnitude() {
+		$norm = sqrt(pow($this->getX(), 2) + pow($this->getY(), 2) + pow($this->getZ(), 2) + pow($this->getW(), 2));
+		return $norm;
 	}
 
-	private function _setY($newval) {
-		$this->_y = $newval;
-		return;
-	}
-	
-	private function _setZ($newval) {
-		$this->_z = $newval;
-		return;
-	}
-	
-	private function _setW($newval) {
-		$this->_w = $newval;
+	private function _diff_vertex() {
+		$this->_x = $this->_dest->getX() - $this->_orig->getX();
+		$this->_y = $this->_dest->getY() - $this->_orig->getY();
+		$this->_z = $this->_dest->getZ() - $this->_orig->getZ();
+		$this->_w = $this->_dest->getW() - $this->_orig->getW();
 		return;
 	}
 	
--- d06/ex02/test.php
+++ d06/ex02/test.php
@@ -41,7 +41,7 @@ $dest2 = new Vertex( array( 'x' => -12.34, 'y' => 23.45, 'z' => -34.56 ) );
 $vtc2  = new Vector( array( 'orig' => $orig2, 'dest' => $dest2 ) );
 
 print( 'Magnitude is ' . $vtc2->magnitude() . PHP_EOL );
-
+/*
 $nVtc2 = $vtc2->normalize();
 print( 'Normalized $vtc2 is ' . $nVtc2 . PHP_EOL );
 print( 'Normalized $vtc2 magnitude is ' . $nVtc2->magnitude() . PHP_EOL );
@@ -55,5 +55,5 @@ print( 'cross product of $vtc1 and $vtc2 is ' . $vtc1->crossProduct( $vtc2 ) . P
 print( 'cross product of $vtcXunit and $vtcYunit is ' . $vtcXunit->crossProduct( $vtcYunit ) . 'aka $vtcZunit' . PHP_EOL );
 print( 'cosinus of angle between $vtc1 and $vtc2 is ' . $vtc1->cos( $vtc2 ) . PHP_EOL );
 print( 'cosinus of angle between $vtcXunit and $vtcYunit is ' . $vtcXunit->cos( $vtcYunit ) . PHP_EOL );
-
+*/
 ?>
