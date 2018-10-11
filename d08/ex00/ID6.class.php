065efb9ac21937584ffaed0d611932dffe9364cc
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-10-11 10:57:54 +0200

    ID6

---
+++ d08/ex00/ID6.class.php
@@ -0,0 +1,10 @@
+<?php
+	interface ID6 {
+		public function getVal(); //private $_val = 0;
+
+		public function launch();
+		public function random(); //int rand (1, 6)
+
+		public static function doc();
+	}
+?>
