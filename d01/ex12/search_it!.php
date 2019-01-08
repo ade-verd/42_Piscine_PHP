f65b7d7e92b013c19f065e4f1318e8e14adcf80d
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2019-01-08 12:58:00 +0100

    fix when there is no value

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -1,21 +1,18 @@
 #!/usr/bin/php
 <?php
-	if ($argc == 2)
+	if ($argc > 2)
 	{
-		if ($argv[1] == "moyenne")
-			average();
-		elseif ($argv[1] == "moyenne_user")
-			average_user();
-		elseif ($argv[1] == "ecart_moulinette")
-			mouli_gap();
-
+		$tosearch = $argv[1];
 		$i = 2;
 
 		$array = array();
 		while ($i < $argc)
 		{
-			list($key, $value) = explode(':', $argv[$i]);
-			$array[$key] = $value;
+			if (strpos($argv[$i], ':') !== FALSE)
+			{
+				list($key, $value) = explode(':', $argv[$i]);
+				$array[$key] = $value;
+			}
 			$i++;
 		}
 		if (array_key_exists($tosearch, $array) === TRUE)
