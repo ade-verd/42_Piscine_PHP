b708998913a5923b38f9502662d4f5b85fa2ae4d
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2019-01-18 14:02:08 +0100

    yess

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,4 +1,3 @@
-document.name = "ppkcookie=todolist; path/";
 
 function new_todo(insert_into_id) {
 	var txt;
@@ -9,51 +8,48 @@ function new_todo(insert_into_id) {
 		var list_item = document.createElement('div');
 		var added_at = "<i><small>[added at " + utcString + "]</small></i>";
 		list_item.innerHTML = todo + " " + added_at;
-		list_item.onclick = function () {
-			if (confirm('Do you really want to remove this todo task ?'))
-				this.parentElement.removeChild(this);
-		};
+		list_item.setAttribute("onclick", "rmChild(this);");
 		document.getElementById(insert_into_id).insertBefore(list_item, document.getElementById(insert_into_id).firstChild);
 		var html =  encodeURIComponent(document.getElementById(insert_into_id).innerHTML);
-		setCookie("todo", html, 3600 * 1000);
-	//	dump(listCookies());
+		createCookie("todo", html, 1);
 		//jQuery('#list_ID:first-child').prepend(new_item);
 	}
 }
 
-function setCookie(cname, cvalue, time) {
-    var d = new Date();
-    d.setTime(d.getTime() + time);
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-    console.log(cname + "=" + cvalue + "; " + expires);
+function rmChild(which)
+{
+	if (confirm('Do you really want to remove this todo task ?'))
+		which.parentElement.removeChild(which);
 }
 
-function check() {
-	var task = document.getElementById('ft_list');
-	task.innerHTML = decodeURIComponent(getCookie('todo'));
+function checkCookies(div_id) {
+	var cook = readCookie("todo");
+	console.log("cookie readed (encoded): " + cook);
+	console.log("cookie readed (decoded): " + decodeURIComponent(cook));
+	document.getElementById(div_id).innerHTML = decodeURIComponent(cook);
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-			return c.substring(name.length,c.length);
-        }
-    }
-    return "";
-}
-
-function dump(obj) {
-	var out = '';
-	for (var i in obj) {
-		out += i + ": " + obj[i] + "\n";
+function createCookie(name, value, days) {
+	if (days)
+	{
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
 	}
+	else
+		var expires = "";
+	document.cookie = name + "=" + value + expires + "; path=/";
+	console.log("cookie created (encoded): " + name + "=" + value + expires + "; path=/");
+}
 
-	console.log(out);
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		console.log(i + ": |" + ca[i] + "|");
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return "";
 }
